bddd8ba5808c308385a796dcd31a89cb2050b606
Gordon Tillman <gordyt@gmail.com>
2017-11-22 10:16:53 -0600

    Enable sshd on mailbox & proxy

--- proxy/entry-point.pl
+++ proxy/entry-point.pl
@@ -158,6 +158,8 @@ EntryExec(
 
       sub { { wait_for_service_to_resolve => $MAILBOX_HOST, }; },
 
+      sub { { desc => "Starting ssh-server", exec => { user => "root", args => [ "/usr/sbin/service", "ssh", "start" ], }, }; },
+
       # FIXME - requires LDAP
       sub { { desc => "Bringing up all services", exec => { args => [ "/opt/zimbra/bin/zmcontrol", "start" ], }, }; },
 
