d84a47cf23048c235d83ff4f7d5200a771d233e2
Gordon Tillman <gordyt@gmail.com>
2017-12-13 12:13:03 -0800

    generate/publish ssh keys

--- mailbox/entry-point.pl
+++ mailbox/entry-point.pl
@@ -310,6 +310,26 @@ EntryExec(
 
       #######################################################################
 
+      sub {
+         {
+            desc => "Running zmsshkeygen",
+            exec => {
+               args => [ "/opt/zimbra/bin/zmsshkeygen" ]
+            },
+         };
+      },
+
+      sub {
+         {
+            desc => "Running zmupdateauthkeys",
+            exec => {
+               args => [ "/opt/zimbra/bin/zmupdateauthkeys" ]
+            },
+         };
+      },
+
+      #######################################################################
+
       sub { { publish_service => {}, }; },
 
       #######################################################################
