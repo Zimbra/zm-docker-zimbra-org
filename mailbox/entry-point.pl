f6c5c7349d36c7bab3b211a26cb44b7b1a27dd70
Gordon Tillman <gordyt@gmail.com>
2018-02-05 17:44:54 -0600

    ZIMBRA-879 Configure zimbraEventBackendURL

--- mailbox/entry-point.pl
+++ mailbox/entry-point.pl
@@ -245,6 +245,7 @@ EntryExec(
          };
       },
 
+      # FIXME - When we add support for SOLR cloud, update zimbraIndexURL and zimbraEventBackendURL
       sub {
          {
             global_config => {
@@ -255,6 +256,7 @@ EntryExec(
                zimbraRemoteImapSSLServerEnabled            => "TRUE",
                zimbraSolrReplicationFactor                 => "1",
                zimbraIndexURL                              => "solr:http://$SOLR_HOST:8983/solr",
+               zimbraEventBackendURL                       => "solr:http://$SOLR_HOST:8983/solr",
             },
          };
       },
