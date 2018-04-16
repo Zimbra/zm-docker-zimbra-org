6104f3be5504f83bce6a0ed693d26cdc18b0d340
Eric Schoonover <eric@teamsodo.com>
2018-04-15 22:51:29 -0700

    add HTML form to account flask app

--- account/src/app.py
+++ account/src/app.py
@@ -1,30 +1,41 @@
 from __future__ import with_statement
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_from_directory
 from fabric.api import *
 from fabric.contrib.console import confirm
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    accountInfo = request.get_json(force=True)
-    user = accountInfo['user']
-    password = accountInfo['password']
-    email = accountInfo['email']
+    return send_from_directory('static', 'index.html')
 
-    if user:
-        createUserCommand = "zmprov ca {} {} displayName {}".format(email, password, user)
-    else:
-        createUserCommand = "zmprov ca {} {}".format(email, password)
+@app.route('/register', methods=['POST'])
+def register():
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    displayName = ''
+    if firstName:
+        displayName += firstName
+    if lastName:
+        displayName += lastName
+
+    userName = request.form['userName']
+    email = userName
+
+    password = "abc123"
+
+    recoveryEmail = request.form['recoveryEmail']
+
+    createUserCommand = "zmprov ca {} {} displayName {}".format(email, password, displayName)
 
     # run the command remotly through ssh via a fabric's high -level library
     with settings(host_string="zmc-mailbox", port=23 ,user="zimbra", password="zimbra", warn_only=True):
         result = run(createUserCommand)
-    
+
     if result.failed:
-        return "Something was wrong :( " + result
+        return "Something was wrong :( " + result, 500
     else:
-        return "Your account was created successfuly " + result
+        return '', 204
 
 
 if __name__ == '__main__':
